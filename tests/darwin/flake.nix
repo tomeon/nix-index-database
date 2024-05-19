dfbb28f9a540bf231da3ec0e4bffa58385339a48
Matt Schreiber <tomeon@dogea.red>
2024-05-19 15:21:13 -0400

    feat: add Darwin test GitHub Actions workflow

--- tests/darwin/flake.nix
+++ tests/darwin/flake.nix
@@ -24,9 +24,9 @@
         in
         {
           run-darwin-test = pkgs.writers.writeBashBin "nix-index-database-run-darwin-test" ''
-            set -x
+            set -eux
 
-            if ! toplevel="$(${pkgs.git}/bin/git rev-parse --show-toplevel --path-format=absolute 2>/dev/null)" || [[ -z "''${toplevel:-}" ]]; then
+            if ! toplevel="$(${pkgs.gitMinimal}/bin/git rev-parse --show-toplevel --path-format=absolute 2>/dev/null)" || [[ -z "''${toplevel:-}" ]]; then
               toplevel="$(${pkgs.coreutils}/bin/readlink -f "''${PWD:-$(pwd -P)}/../..")"
             fi
 
@@ -35,29 +35,49 @@
               | ${pkgs.jq}/bin/jq --raw-output '.locks.nodes.nixpkgs.locked | "\(.type):\(.owner)/\(.repo)/\(.rev)"'
             )"
 
-            ${nix-darwin.packages.${system}.darwin-rebuild}/bin/darwin-rebuild \
+            # Otherwise:
+            # > error: Unexpected files in /etc, aborting activation
+            # > The following files have unrecognized content and would be overwritten:
+            # >   /etc/nix/nix.conf
+            # > Please check there is nothing critical in these files, rename them by adding .before-nix-darwin to the end, and then try again.
+            sudo mv -f /etc/nix/nix.conf{,.before-nix-darwin} || :
+
+            sudo ${nix-darwin.packages.${system}.darwin-rebuild}/bin/darwin-rebuild \
               --override-input nix-index-database "$toplevel" \
               --override-input nixpkgs "$nixpkgs_url" \
               -L \
-              '.#${system}'
+              --flake '.#${system}' \
+              switch
+
+            # Source this file to ensure `nix-locate` available.
+            # Disable errexit and nounset since `/etc/static/bashrc` is not
+            # compatible with "strict mode".
+            set +eu
+            source /etc/static/bashrc || :
+            set -eu
 
             if (( "$#" < 1 )); then
               set -- "''${BASH:-bash}" -c 'nix-locate --top-level --whole-name --at-root /bin/rg | cut -d" " -f1 | grep -F "ripgrep.out"'
             fi
 
-            exec "$@"
+            "$@"
           '';
         });
 
       darwinConfigurations = lib.genAttrs testSystems (system:
         nix-darwin.lib.darwinSystem {
           modules = [
-            nix-index-database.darwinModules.nix-index-database
+            nix-index-database.darwinModules.nix-index
 
             {
               networking.hostName = "nix-index-darwin-test";
               nixpkgs.hostPlatform = system;
-              programs.command-not-found.enable = false;
+
+              # Without this, `nix-darwin` carps:
+              # > error: The daemon is not enabled but this is a multi-user install, aborting activation
+              # > Enable the nix-daemon service:
+              # >     services.nix-daemon.enable = true;
+              services.nix-daemon.enable = true;
             }
           ];
         });
